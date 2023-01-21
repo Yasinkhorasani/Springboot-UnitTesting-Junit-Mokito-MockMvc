1ef909215fe5dbd337f02696da0199efd918a32f
Yasin khorasani <yasinkhorasani@yahoo.com>
2023-01-21 16:50:55 +0100

    Integration/JunitTest/Return List of Movies

--- src/test/java/springboot/Junit/integration/MoviesIntegrationTest.java
+++ src/test/java/springboot/Junit/integration/MoviesIntegrationTest.java
@@ -10,9 +10,9 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 import springboot.Junit.model.Movie;
 import springboot.Junit.repository.MovieRepository;
-
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -54,5 +54,27 @@ public class MoviesIntegrationTest {
 
         assertNotNull(newMovie);
         assertThat(newMovie.getId()).isNotNull();
+    }
+
+    ////////////////////////////////////////////////////////////
+    @Test
+    public void shouldFetchMovieTest(){
+        Movie avatarMovie = new Movie();
+        avatarMovie.setName("Avatar");
+        avatarMovie.setGenera("Action");
+        avatarMovie.setReleaseDate(LocalDate.of(2000, Month.APRIL,22));
+
+        Movie titanicMovie = new Movie();
+        titanicMovie.setName("Titanic");
+        titanicMovie.setGenera("Romantic");
+        titanicMovie.setReleaseDate(LocalDate.of(2003, Month.MARCH,12));
+
+        restTemplate.postForObject(baseURL,avatarMovie, Movie.class);
+        restTemplate.postForObject(baseURL,titanicMovie, Movie.class);
+
+        List<Movie> movieList = restTemplate.getForObject(baseURL,List.class);
+
+        assertThat(movieList.size()).isEqualTo(2);
+
     }
 }
